d548a71838378ef8aa334dbe602ced04602c1b01
Raftouch <rafanelkin@gmail.com>
2023-09-30 18:41:46 +0200

    :bento: updated navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,6 +12,20 @@ export default function Navbar() {
         Home
       </Link>
       <div className="flex gap-5">
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0} className="cursor-pointer">
+            Topics
+          </label>
+          <ul
+            tabIndex={0}
+            className="dropdown-content z-[1] menu p-4 bg-slate-200 rounded-box gap-2"
+          >
+            <Link href="/topics/health">Health</Link>
+            <Link href="/topics/dogs">Dogs</Link>
+            <Link href="/topics/coding">Coding</Link>
+          </ul>
+        </div>
+
         <Link
           href="/static"
           className={pathname === '/static' ? 'text-green-600' : ''}
